acb4a2ed3ee37457c3bbf9bd9cc15e35ca6ef9e1
mid <1627115770@qq.com>
2025-07-07 01:49:07 +0800

    77add

--- src/eval/BLIPvqa_eval/test.sh
+++ src/eval/BLIPvqa_eval/test.sh
@@ -2,5 +2,5 @@ export project_dir="src/eval/BLIPvqa_eval/"
 cd $project_dir
 data="SKCI"
 out_dir="../../../examples/${data}"
-HF_ENDPOINT=https://hf-mirror.com \ # Set the Hugging Face endpoint（可选）
+HF_ENDPOINT=https://hf-mirror.com \ Set the Hugging Face endpoint（可选）
 python BLIP_vqa.py --out_dir=$out_dir
